d03e3922bcf501785d0b2cf285ad4442142c3031
Joon Hyub Lee <joonhyub.lee@gmail.com>
2017-06-01 19:31:56 +0000

    kakao fake app working

--- app.js
+++ app.js
@@ -2,6 +2,196 @@ import React    from 'react';
 import ReactDOM from 'react-dom';
 import io       from 'socket.io-client';
 
+const style = {
+    container: {
+        backgroundColor: '#a0c0d7',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'flex-end',
+        fontFamily: 'sans-serif',
+        paddingBottom: 120
+    },
+
+    top_status_bar: {
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: 40,
+        backgroundColor: 'rgba(160, 192, 215, 0.95)',
+    },
+
+    top_bar: {
+        position: 'fixed',
+        top: 40,
+        left: 0,
+        width: '100%',
+        height: 90, // subtract system status bar height
+        backgroundColor: 'rgba(160, 192, 215, 0.95)',
+        borderBottom: '1px solid rgb(152, 182, 204)',
+        display: 'flex'
+    },
+
+    top_bar_left: {
+        // backgroundColor: 'beige',
+        width: '33.3%',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'flex-start'
+    },
+
+    top_bar_center: {
+        // backgroundColor: 'pink',
+        width: '33.3%',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        fontSize: 35,
+        fontWeight: 600
+    },
+
+    top_bar_right: {
+        // backgroundColor: 'green',
+        width: '33.3%',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'flex-end'
+    },
+
+    top_bar_left_icon: {
+        marginLeft: 35,
+        width: 40,
+        height: 40,
+        // border: '1px solid black'
+    },
+
+    top_bar_right_icon: {
+        marginRight: 35,
+        width: 40,
+        height: 40,
+        // border: '1px solid black'
+    },
+
+    chat_container: {
+        display: 'flex'
+    },
+
+    chat_info_column: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'flex-end',
+        textAlign: 'right',
+        marginRight: 10,
+        fontSize: 20
+    },
+
+    chat_info_unread_count: {
+        color: '#ffeb00',
+        textShadow: '1px 1px #859fb2'
+    },
+
+    chat_info_time: {
+        color: '#657988'
+    },
+
+    chat_bubble: {
+        maxWidth: 500,
+        backgroundColor: '#ffeb00',
+        marginTop: 10,
+        // minHeight: 70,
+        padding: '17px 15px 17px 15px', // 18px
+        fontSize: 30,
+        borderRadius: 5
+    },
+
+    chat_bubble_head: {
+        width: 35
+    },
+
+    text_bar: {
+        position: 'fixed',
+        bottom: 0,
+        left: 0,
+        width: '100%',
+        height: 100,
+        backgroundColor: '#eeeeee',
+        display: 'flex',
+        alignItems: 'center'
+    },
+
+    text_bar_left_icon: {
+        width: 40,
+        height: 40,
+        // border: '1px solid black',
+        marginLeft: 20,
+        marginRight: 20
+    },
+
+    text_bar_input_container: {
+        height: 64,
+        flexGrow: 1,
+        borderRadius: 5,
+        border: '1px solid #d7d7d7',
+        backgroundColor: 'white',
+        marginRight: 12,
+        display: 'flex',
+        alignItems: 'center'
+    },
+
+    text_bar_input: {
+        height: 64,
+        flexGrow: 1,
+        fontSize: 30,
+        display: 'flex',
+        alignItems: 'center',
+        paddingLeft: 17
+    },
+    
+    text_bar_right_icon: {
+        width: 40,
+        height: 40,
+        // border: '1px solid black',
+        marginRight: 20
+    },
+
+    text_bar_send_button: {
+        width: 80,
+        height: 64,
+        backgroundColor: '#ffeb00',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        fontSize: 26,
+        color: '#736b24',
+        borderTopRightRadius: 5,
+        borderBottomRightRadius: 5,
+        borderLeft: '1px solid #d7d7d7',
+        fontWeight: 600
+    }
+}
+
+const convert_time = time_obj => {
+    let timeString = '';
+
+    let firstString = '오전';
+    let hours = time_obj.getHours();
+    let mins = time_obj.getMinutes();
+
+    if (hours > 12) {
+        hours = hours - 12; firstString = '오후';
+    }
+
+    hours = hours + '';
+    mins = mins + ''; 
+
+    if (mins.length < 2) {
+        mins = '0' + mins;
+    }
+
+    timeString = firstString + ' ' + hours + ':' + mins;
+    return timeString;
+}
+
 const read_all_unread_messages = message_list => {
     console.log('[front] reading mode');
 
@@ -29,6 +219,7 @@ class App extends React.Component {
         if (document.location.pathname == '/read') {
             read_all_unread_messages(this.props.message_list);
         }
+        window.scrollTo(0,document.body.scrollHeight);
     }
 
     componentDidUpdate() {
@@ -37,33 +228,156 @@ class App extends React.Component {
         if (document.location.pathname == '/read') {
             read_all_unread_messages(this.props.message_list);
         }
+        window.scrollTo(0,document.body.scrollHeight);
     }
 
     render() {
         return (
-            <div>
-                Hello World!
+            <div style={ style.container }>
+                <div style={ style.top_status_bar }/>
+
+                <div style={ style.top_bar }>
+                    <div style={ style.top_bar_left }>
+                        <div style={ style.top_bar_left_icon }>
+                            <svg
+                                width="40"
+                                height="40"
+                                viewBox="0 0 40 40"
+                            >
+                                <path
+                                    fill="none"
+                                    strokeWidth="4"
+                                    stroke="#000000"
+                                    d="M 20 0 L 0 20 L 20 40"
+                                />
+                            </svg>
+                        </div>
+                    </div>
+
+                    <div style={ style.top_bar_center }>
+                        안상균
+                    </div>
+
+                    <div style={ style.top_bar_right }>
+                        <div style={ style.top_bar_right_icon }>
+                            <svg
+                                width="40"
+                                height="40"
+                                viewBox="0 0 40 40"
+                            >
+                                <ellipse
+                                    cx="15"
+                                    cy="15"
+                                    rx="15"
+                                    ry="15"
+                                    fill="none"
+                                    strokeWidth="4"
+                                    stroke="#000000"
+                                />
+                                <path
+                                    fill="none"
+                                    strokeWidth="4"
+                                    stroke="#000000"
+                                    d="M 25 25 L 40 40"
+                                />
+                            </svg>
+                        </div>
+
+                        <div style={ style.top_bar_right_icon }>
+                            <svg
+                                width="40"
+                                height="40"
+                                viewBox="0 0 40 40"
+                            >
+                                <path
+                                    fill="none"
+                                    strokeWidth="4"
+                                    stroke="#000000"
+                                    d="M 0 0 L 40 0 M 0 20 L 40 20 M 0 40 L 40 40"
+                                />
+                            </svg>
+                        </div>
+                    </div>
+                </div>
+
                 { this.props.message_list.map((message, i) => (
-                    <div key={ i }>
-                        { new Date(message.time) + '' }
-                        { message.content }
-                        { message.is_read ? '' : '1' }
+                    <div
+                        key={ i }
+                        style={ style.chat_container }
+                    >
+                        <div style={ style.chat_info_column }>
+                            <div style={ style.chat_info_unread_count }>
+                                { message.is_read ? '' : '1' }
+                            </div>
+
+                            <div style={ style.chat_info_time }>
+                                { convert_time(new Date(message.time)) }
+                            </div>
+                        </div>
+
+                        <div style={ style.chat_bubble }>
+                            { message.content }
+                        </div>
+
+                        <div style={ style.chat_bubble_head }>
+                        </div>
                     </div>
                 ))}
 
-                <br/>
-                <input type="text" ref={ node => this.chat_box = node }/>
+                <div style={ style.text_bar }>
+                    <div style={ style.text_bar_left_icon }>
+                        <svg
+                            width="40"
+                            height="40"
+                            viewBox="0 0 40 40"
+                        >
+                            <path
+                                fill="none"
+                                strokeWidth="4"
+                                stroke="#a0a0a0"
+                                strokeLinecap="round"
+                                d="M 5 20 L 35 20 M 20 5 L 20 35"
+                            />
+                        </svg>
+                    </div>
 
-                <div onClick={ () => {
-                    const message_content = this.chat_box.value
-                    console.log('[front] writing one message:', message_content);
+                    <div style={ style.text_bar_input_container }>
+                        <div
+                            contentEditable={ true }
+                            ref={ node => this.chat_box = node }
+                            style={ style.text_bar_input }
+                        />
 
-                    if (this.chat_box.value.length > 0) {
-                        socket.emit('write_one_message', message_content);
-                        this.chat_box.value = '';
-                    }
-                }}>
-                    Send
+                        <div style={ style.text_bar_right_icon }>
+                            <svg
+                                width="40"
+                                height="40"
+                                viewBox="0 0 40 40"
+                            >
+                                <ellipse
+                                    cx="20" cy="20" rx="20" ry="20"
+                                    fill="none"
+                                    strokeWidth="4"
+                                    stroke="#a0a0a0"
+                                />
+                            </svg>
+                        </div>
+
+                        <div
+                            onClick={() => {
+                                const message_content = this.chat_box.textContent;
+                                console.log('[front] writing one message:', message_content);
+
+                                if (this.chat_box.textContent.length > 0) {
+                                    socket.emit('write_one_message', message_content);
+                                    this.chat_box.textContent = '';
+                                }
+                            }}
+                            style={ style.text_bar_send_button }
+                        >
+                            전송
+                        </div>
+                    </div>
                 </div>
             </div>
         );
